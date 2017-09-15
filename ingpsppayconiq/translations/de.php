1b7783b4635859230bbe0a346964e10557ab2696
Ruslan Dmitrijev <ruslan@dmitrijev.net>
2017-09-15 13:39:04 +0300

    Updated webhook

---
+++ ingpsppayconiq/translations/de.php
@@ -0,0 +1,16 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_d047708e9dbab4f7c9bfeba9121c54f0'] = 'ING PSP Payconiq';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_d442ffc36743a9c04e02f98cef21dcda'] = 'Akzeptiere Zahlungen für Ihre Produkte mit Payconiq';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_cbe0a99684b145e77f3e14174ac212e3'] = 'Sind Sie sicher, diese Details zu entfernen?';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_a02758d758e8bec77a33d7f392eb3f8a'] = 'Keine Währung ist für dieses Modul eingestellt worden. Sie können das einstellen in das Localization / Currencies Menu.';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_e58b17c25dbd18daa157d79957e8910c'] = 'Bezahlen bei Payconiq';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_7853e11acea9a6109b2f1c00cdb4041e'] = 'Ihre Bestellung bei';
+$_MODULE['<{ingpsppayconiq}prestashop>error_5023dbc72719b0d03162a18fda6db2b3'] = 'Leider ist ein Fehler bei der Verarbeitung Ihrer Bezahlung aufgetreten.';
+$_MODULE['<{ingpsppayconiq}prestashop>error_7dc1ee1a41f69f4027a773780c058b07'] = 'Bitte klick hier um eine andere Zahlungsart zu wahlen.';
+$_MODULE['<{ingpsppayconiq}prestashop>error_34e0a062726d06e24047764c3bef28c0'] = 'Bitte klicken Sie hier noch einmal zu versuchen.';
+$_MODULE['<{ingpsppayconiq}prestashop>error_b260f74ed85604be0f0381ce059f6b44'] = 'Kehren Sie zurück zur Kassa.';
+$_MODULE['<{ingpsppayconiq}prestashop>payment_e58b17c25dbd18daa157d79957e8910c'] = 'Bezahlen bei Payconiq';
+$_MODULE['<{ingpsppayconiq}prestashop>payment_return_0f9b4be6207d496fc281b0cce2eeed3b'] = 'Herzlichen Dank für Ihre Bestellung.';
---
+++ ingpsppayconiq/translations/nl.php
@@ -0,0 +1,16 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_d047708e9dbab4f7c9bfeba9121c54f0'] = 'ING PSP Payconiq';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_d442ffc36743a9c04e02f98cef21dcda'] = 'Met deze module kunt u Payconiq betalingen accepteren.';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_cbe0a99684b145e77f3e14174ac212e3'] = 'Weet u zeker dat u deze gegevens wenst te verwijderen?';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_a02758d758e8bec77a33d7f392eb3f8a'] = 'Er is geen valuta ingesteld voor deze module. U kunt dit instellen in het Localization / Currencies menu.';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_e58b17c25dbd18daa157d79957e8910c'] = 'Betaal met Payconiq';
+$_MODULE['<{ingpsppayconiq}prestashop>ingpsppayconiq_7853e11acea9a6109b2f1c00cdb4041e'] = 'Uw bestelling bij';
+$_MODULE['<{ingpsppayconiq}prestashop>error_5023dbc72719b0d03162a18fda6db2b3'] = 'Helaas is er een fout opgetreden tijdens het verwerken van uw betaling.';
+$_MODULE['<{ingpsppayconiq}prestashop>error_7dc1ee1a41f69f4027a773780c058b07'] = 'Klik hier om een andere betaalmethode te selecteren.';
+$_MODULE['<{ingpsppayconiq}prestashop>error_34e0a062726d06e24047764c3bef28c0'] = 'Klik hier om nogmaals te proberen.';
+$_MODULE['<{ingpsppayconiq}prestashop>error_b260f74ed85604be0f0381ce059f6b44'] = 'Keer terug naar de betaalpagina.';
+$_MODULE['<{ingpsppayconiq}prestashop>payment_e58b17c25dbd18daa157d79957e8910c'] = 'Betaal met Payconiq';
+$_MODULE['<{ingpsppayconiq}prestashop>payment_return_0f9b4be6207d496fc281b0cce2eeed3b'] = 'Dank voor uw bestelling';
